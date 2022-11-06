8301ea98c868dd1a1ad89d831fac47c784da63e5
Дима Ларионов <dima_larionov_000@mail.ru>
2022-11-06 19:09:12 +0300

    Добавьте файлы проекта.

---
+++ staples/Program.cs
@@ -0,0 +1,34 @@
+﻿using staples;
+
+Staples staples=new Staples();
+while (true)
+{
+    if (staples.ReturnStaples() == false)
+    {
+        for (int i = 20; i >= 0; i--)
+        {
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("Думаю.....");
+            if (i == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Неправильно, попробуй ещё раз(False)");
+                Console.WriteLine();
+            }
+        }
+    }
+    else 
+    {
+        for (int i = 20; i >= 0; i--)
+        {
+            System.Threading.Thread.Sleep(200);
+            Console.WriteLine("Думаю.....");
+            if (i == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Так уж и быть, правильно(True)");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ staples/Staples.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staples;
+
+public class Staples
+{
+    Stack<char> stapl;
+    public Staples()
+    {
+        stapl = new Stack<char>();
+    }
+    public bool ReturnStaples()
+    {
+        Console.WriteLine("Введите скобки для проверки их закрытия");
+        string select = Console.ReadLine();
+        if (select == null || select == " ")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите корректную команду");
+            Console.WriteLine();
+            return false;
+        }
+        else
+        {
+            foreach (char staples in select)
+            {
+                if (staples == '(' || staples == '{' || staples == '[')
+                {
+                    stapl.Push(staples);
+                }
+                else if (staples == ')')
+                {
+                    if (stapl.Count == 0) { return false; }
+                    else if (stapl.Pop() != '(') { return false; }
+                }
+                else if (staples == '}')
+                {
+                    if (stapl.Count == 0) { return false; }
+                    else if (stapl.Pop() != '{') { return false; }
+                }
+                else if (staples == ']')
+                {
+                    if (stapl.Count == 0) { return false; }
+                    else if (stapl.Pop() != '[') { return false; }
+                }
+            }
+                return stapl.Count() == 0; 
+        }
+    }
+}
